c1e03109af319092a4fdacd88cf1b70ed345a710
Zac McEwen <Zac@Zacs-MacBook-Pro.local>
2017-08-16 04:36:41 +0930

    Initial commit

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dragable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
